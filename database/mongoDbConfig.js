c756fc995b8d2836ce046c8d80d581184290a041
kabindrr <chaina_kabindra@yahoo.com>
2024-12-12 10:18:06 +1100

    db config

--- database/mongoDbConfig.js
+++ database/mongoDbConfig.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const uri = process.env.MONGO_URL;
 
-export const MongoDb = () => {
+export const MongoDbConfig = async () => {
   try {
-    mongoose.connect(uri);
+    console.log(uri);
+    await mongoose.connect(uri);
+    // await mongoose.connect("mongodb://localhost:27017/lmsdb2");
     console.log("Database connected with mongoDB");
   } catch (error) {
     console.log(error.message);
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 import express, { json } from "express";
 import cors from "cors";
-import { MongoDb } from "./database/mongoDbConfig.js";
+import { MongoDbConfig } from "./database/mongoDbConfig.js";
 import morgan from "morgan";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 //Database
-MongoDb();
 
 //middlewares
 app.use(cors());
@@ -22,8 +23,14 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(PORT, (error) => {
-  error
-    ? console.log("Server error")
-    : console.log(`Server Connected at http://localhost:${PORT}`);
-});
+MongoDbConfig()
+  .then((error) => {
+    app.listen(PORT, (error) => {
+      error
+        ? console.log("Server error")
+        : console.log(`Server Connected at http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
